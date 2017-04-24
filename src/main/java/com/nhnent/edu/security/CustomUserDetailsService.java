90cbc630359947d05c5f8eb7178c20b6e42cd516
신동민/NE <dongmyo@nhnent.com>
2017-04-25 06:55:14 +0900

    * implements CustomUserDetailsService

--- src/main/java/com/nhnent/edu/security/CustomUserDetailsService.java
+++ src/main/java/com/nhnent/edu/security/CustomUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.nhnent.edu.security;
 
 import com.nhnent.edu.dao.MemberDao;
+import com.nhnent.edu.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,14 +20,22 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String password = null;
         List<GrantedAuthority> authorities = new ArrayList<>();
 
-        /*
-         * TODO : #8 직접 구현 하세요.
-         */
+        Member member = memberDao.exists(username);
 
-        return new User(username, password, authorities);
+        if (member == null) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        String authority = memberDao.getAuthority(username);
+        if (authority == null) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        authorities.add(new SimpleGrantedAuthority(authority));
+
+        return new User(username, member.getPassword(), authorities);
     }
 
 }
