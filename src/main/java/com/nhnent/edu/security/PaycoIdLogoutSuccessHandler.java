006e7a25669ac28beb1b65252669474f1e1a71ea
신동민/NE <dongmyo@nhnent.com>
2017-10-13 05:41:51 +0900

    no message

--- src/main/java/com/nhnent/edu/security/PaycoIdLogoutSuccessHandler.java
+++ src/main/java/com/nhnent/edu/security/PaycoIdLogoutSuccessHandler.java
@@ -3,6 +3,8 @@ package com.nhnent.edu.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
@@ -12,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-// TODO : #2 LogoutSuccessHandler 구현
+// TODO : #3 LogoutSuccessHandler 구현
 public class PaycoIdLogoutSuccessHandler implements LogoutSuccessHandler {
     @Autowired
     OAuth2RestTemplate oAuth2RestTemplate;
@@ -20,16 +22,31 @@ public class PaycoIdLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        /*
+         * TODO : #6 구현하세요
+         * cf.)
+         *  PAYCO ID 연동 가이드 : https://developers.payco.com/guide/development/apply
+         *  PAYCO ID Access Token 삭제 API : https://id.payco.com/oauth2.0/logout
+         */
         String clientId = oAuth2RestTemplate.getResource().getClientId();
-        String token = oAuth2RestTemplate.getAccessToken().getValue();
 
-        String paycoIdLogoutUrl = "https://id.payco.com/oauth2.0/logout";
+        String token = Optional.ofNullable(authentication)
+                .map(OAuth2Authentication.class::cast)
+                .map(OAuth2Authentication::getUserAuthentication)
+                .map(Authentication::getCredentials)
+                .map(String.class::cast)
+                .orElse(null);
 
-        Map<String, Object> params = new HashMap<>();
-        params.put("client_id", clientId);
-        params.put("token", token);
+        if (token != null && token.length() > 0) {
+            String paycoIdLogoutUrl = "https://id.payco.com/oauth2.0/logout";
 
-        oAuth2RestTemplate.getForObject(paycoIdLogoutUrl, Map.class, params);
+            Map<String, Object> params = new HashMap<>();
+            params.put("client_id", clientId);
+            params.put("token", token);
+
+            oAuth2RestTemplate.getOAuth2ClientContext().setAccessToken(new DefaultOAuth2AccessToken(token));
+            oAuth2RestTemplate.getForObject(paycoIdLogoutUrl, Map.class, params);
+        }
 
         request.getSession().invalidate();
 
