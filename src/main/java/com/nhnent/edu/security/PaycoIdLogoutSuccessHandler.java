96d48a0d761976c413ede02c6795c04d7fb0f1c5
신동민/NE <dongmyo@nhnent.com>
2017-10-13 05:04:02 +0900

    * LogoutSuccessHandler

--- src/main/java/com/nhnent/edu/security/PaycoIdLogoutSuccessHandler.java
+++ src/main/java/com/nhnent/edu/security/PaycoIdLogoutSuccessHandler.java
@@ -3,8 +3,6 @@ package com.nhnent.edu.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
@@ -13,9 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
-// TODO : #3 LogoutSuccessHandler 구현
+// TODO : #2 LogoutSuccessHandler 구현
 public class PaycoIdLogoutSuccessHandler implements LogoutSuccessHandler {
     @Autowired
     OAuth2RestTemplate oAuth2RestTemplate;
@@ -23,12 +20,20 @@ public class PaycoIdLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        /*
-         * TODO : #6 구현하세요
-         * cf.)
-         *  PAYCO ID 연동 가이드 : https://developers.payco.com/guide/development/apply
-         *  PAYCO ID Access Token 삭제 API : https://id.payco.com/oauth2.0/logout
-         */
+        String clientId = oAuth2RestTemplate.getResource().getClientId();
+        String token = oAuth2RestTemplate.getAccessToken().getValue();
+
+        String paycoIdLogoutUrl = "https://id.payco.com/oauth2.0/logout";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("client_id", clientId);
+        params.put("token", token);
+
+        oAuth2RestTemplate.getForObject(paycoIdLogoutUrl, Map.class, params);
+
+        request.getSession().invalidate();
+
+        response.sendRedirect("/");
     }
 
 }
