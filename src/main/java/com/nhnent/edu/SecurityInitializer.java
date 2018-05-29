241d804e1bad6767ab26eee029bd0ca3c969b0aa
신동민/NE <dongmyo@nhnent.com>
2018-05-29 11:25:38 +0900

    * remove web.xml

---
+++ src/main/java/com/nhnent/edu/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.nhnent.edu;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// TODO : #2 remove web.xml - springSecurityFilterChain
+// AbstractSecurityWebApplicationInitializer
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
--- src/main/java/com/nhnent/edu/config/RootContextConfig.java
+++ src/main/java/com/nhnent/edu/config/RootContextConfig.java
@@ -4,18 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
-// TODO: #1 root context configuration
-/*
-<?xml version="1.0" encoding="UTF-8"?>
-<beans xmlns="http://www.springframework.org/schema/beans"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
-
-	<!-- Root Context: defines shared resources visible to all other web components -->
-    <import resource="security.xml" />
-
-</beans>
- */
 @Configuration
 @ComponentScan(basePackages = "com.nhnent.edu", excludeFilters = {@ComponentScan.Filter(Controller.class)})
 public class RootContextConfig {
